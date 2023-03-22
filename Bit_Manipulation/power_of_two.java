03b2c6e6ef643b442266c23f646a7a58a979a8bb
HarshitaJogi <109593461+HarshitaJogi@users.noreply.github.com>
2023-03-22 18:34:58 +0530

    Add files via upload

---
+++ Bit_Manipulation/power_of_two.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        int check = n&(n-1);
+        if(check == 0) return true;
+        else return false;
+    }
+}
